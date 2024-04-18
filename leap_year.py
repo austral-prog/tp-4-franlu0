526b98db96627ba00b8628218955e5fe87e48d6a
franlu0 <162721582+franlu0@users.noreply.github.com>
2024-04-18 16:11:36 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    a=int(input("Ingrese un año:"))
+    if(a%4==0) and (a%100!=0):     
+        print(f"El año {a} es bisiesto") 
+    elif(a%400==0):
+        print(f"El año {a} es bisiesto") 
+    else:
+        print(f"El año {a} no es bisiesto")    
